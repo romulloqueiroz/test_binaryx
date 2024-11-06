e4dedf762cfe136010a217e86708428cb4d0fa16
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2024-11-06 10:54:43 +0900

    fix - input color

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ const Page = () => {
               <input
                 type="email"
                 id="email"
-                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none text-gray-900"
                 placeholder="name@domain.com"
               />
             </div>
